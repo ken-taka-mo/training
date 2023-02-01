85d3500e0f664e9d1ec0b602c4232f58409563d4
user <user@localhost.localdomain>
2023-02-01 13:13:15 +0900

    データを取得し表示

--- company/index.php
+++ company/index.php
@@ -1,15 +1,10 @@
 <?php
+require_once('../dbconnect.php');
+require_once('../utils/functions.php');
+require_once('../utils/prefectures.php');
 
-// if (isset($_GET("search"))) {
-//     $companies = searchの値によってSQLを変える
-// } else {
-//     if($_GET["order"] = "DESC") {
-//         $companies = 降順
-//     } else {
-//         $companies = 昇順
-//     }
-// }
-
+$companies = $db->query('SELECT id, name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address FROM companies');
+$companies = $companies->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +23,7 @@
                 <h1>会社一覧</h1>
             </div>
             <div class="menu">
-                <a href="register.php" class="create">新規登録</a><!-- company/register_company.phpに遷移 -->
+                <a href="register.php" class="create">新規登録</a>
                 <form action="" method="GET">
                     <input type="text" class="search-form">
                     <input class="search" type="submit" value="検索">
@@ -51,6 +46,20 @@
                     <th class="link">編集</th>
                     <th class="link">削除</th>
                 </tr>
+                <?php foreach ($companies as $company) :?>
+                    <tr>
+                        <th><?= h($company['id']) ?></th>
+                        <th><?= h($company['name']) ?></th>
+                        <th><?= h($company['manager_name']) ?></th>
+                        <th><?= h($company['phone_number']) ?></th>
+                        <th><?= '〒' . h($company['postal_code']) . "<br>" . $prefectures[h($company['prefecture_code'])] . h($company['address'])?></th>
+                        <th><?= h($company['mail_address'])?></th>
+                        <th class="link to-list"><a href="../quotations/index.php?id=<?= h($company['id'])?>">見積一覧</a></th><!-- estimation/index.php?=会社番号(ID)で遷移 -->
+                        <th class="link to-list"><a href="../invoices/index.php?id=<?= h($company['id'])?>">請求一覧</a></th><!-- bill/index.php?=会社番号(ID)で遷移 -->
+                        <th class="link"><a href="edit.php?id=<?= h($company['id'])?>">編集</a></th><!-- company/edit_company.php?=会社番号(ID)で遷移-->
+                        <th class="link"><a href="#">削除</a></th><!-- where id = 会社番号でDELETE -->
+                    </tr>
+                <?php endforeach?>
                 <tr>
                     <!-- 会社テーブルのプレフィックス、作成日時、更新日時以外を10行ずつ一覧表示 -->
                     <!-- デフォルトは会社番号昇順で表示 -->
