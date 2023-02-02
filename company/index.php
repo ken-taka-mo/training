fa2a428a0856a0f45a81bc360f63a3fae627a886
user <user@localhost.localdomain>
2023-02-02 12:05:12 +0900

    1ページ10件ずつデータを表示

--- company/index.php
+++ company/index.php
@@ -3,8 +3,34 @@ require_once('../dbconnect.php');
 require_once('../utils/functions.php');
 require_once('../utils/prefectures.php');
 
-$companies = $db->query('SELECT id, name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address FROM companies');
-$companies = $companies->fetchAll();
+$page = 1;
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM companies');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 10);
+if ($maxPage == 0) {
+    $maxPage = 1;
+}
+if (isset($_GET['page'])) {
+    if (!preg_match('/^[0-9]+$/', $_GET['page']) || $_GET['page'] == 0) {
+        header('Location: index.php');
+        exit();
+    } else {
+        if ($_GET['page'] > $maxPage) {
+            header("Location: index.php?page={$maxPage}");
+            exit();
+        } else {
+            $page = $_GET['page'];
+            $page = max($page, 1);
+            $page = min($page, $maxPage);
+        }
+    }
+}
+$start = ($page - 1) * 10;
+
+$statement = $db->prepare('SELECT id, name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address FROM companies LIMIT ?,10');
+$statement->bindParam(1, $start, PDO::PARAM_INT);
+$statement->execute();
+$companies = $statement->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -57,16 +83,22 @@ $companies = $companies->fetchAll();
                         <th><?= h(insertHyphen($company['phone_number'])) ?></th>
                         <th><?= '〒' . h(substr_replace($company['postal_code'], '-', 3, 0)) . "<br>" . $prefectures[h($company['prefecture_code'])] . h($company['address'])?></th>
                         <th><?= h($company['mail_address'])?></th>
-                        <th class="link to-list"><a href="../quotations/index.php?id=<?= h($company['id'])?>">見積一覧</a></th><!-- estimation/index.php?=会社番号(ID)で遷移 -->
-                        <th class="link to-list"><a href="../invoices/index.php?id=<?= h($company['id'])?>">請求一覧</a></th><!-- bill/index.php?=会社番号(ID)で遷移 -->
-                        <th class="link"><a href="edit.php?id=<?= h($company['id'])?>">編集</a></th><!-- company/edit_company.php?=会社番号(ID)で遷移-->
+                        <th class="link to-list"><a href="../quotations/index.php?id=<?= h($company['id'])?>">見積一覧</a></th>
+                        <th class="link to-list"><a href="../invoices/index.php?id=<?= h($company['id'])?>">請求一覧</a></th>
+                        <th class="link"><a href="edit.php?id=<?= h($company['id'])?>">編集</a></th>
                         <th class="link"><a href="delete.php?id=<?= h($company['id'])?>">削除</a></th><!-- where id = 会社番号でDELETE -->
                     </tr>
                 <?php endforeach?>
             </table>
             <div class="page-navigation">
-                <a href="" class="prev p-nav"><span>&larr;</span>前へ</a><!-- 前の10行を表示。なければクリック不可。 -->
-                <a href="" class="next p-nav">次へ<span>&rarr;</span></a> <!-- 次の10行を表示。なければクリック不可。 -->
+                <?php if ($page <= 1) :?>
+                    <a href="index.php?page=<?= $page + 1?>" class="next p-nav">次へ<span>&rarr;</span></a>
+                <?php elseif ($page >= $maxPage) :?>
+                    <a href="index.php?page=<?= $page - 1?>" class="prev p-nav"><span>&larr;</span>前へ</a>
+                <?php else :?>
+                    <a href="index.php?page=<?= $page - 1?>" class="prev p-nav"><span>&larr;</span>前へ</a>
+                    <a href="index.php?page=<?= $page + 1?>" class="next p-nav">次へ<span>&rarr;</span></a>
+                <?php endif?>
             </div>
         </div>
     </main>
