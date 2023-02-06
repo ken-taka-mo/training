caa7a31707b42177e4e4fe604c19a495807186ac
user <user@localhost.localdomain>
2023-02-06 14:35:08 +0900

    論理削除ファイル作成

---
+++ invoices/delete.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../dbconnect.php');
+if (!isset($_GET['no']) || !preg_match('/^[a-zA-Z0-9]{1,8}?(-i-)[0-9]{8}$/', $_GET['no'])) {
+    header('Location: ../companies');
+    exit();
+}
+$no = $_GET['no'];
+$statement = $db->prepare('SELECT id, company_id FROM invoices WHERE no=?');
+$statement->execute(array($no));
+$idArray = $statement->fetch();
+
+if (!$idArray) {
+    header('Location: index.php');
+} else {
+    $deleteStatement = $db->prepare('UPDATE invoices set deleted=NOW() WHERE id=?');
+    $deleteStatement->bindParam(1, $idArray['id'], PDO::PARAM_INT);
+    $deleteStatement->execute();
+    header("Location: index.php?id={$idArray['company_id']}");
+    exit;
+}
