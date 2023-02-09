ad77161b6aeea97d98a31623c8da0795f64ad272
user <user@localhost.localdomain>
2023-02-09 15:07:51 +0900

    h関数使用

--- invoices/check.php
+++ invoices/check.php
@@ -62,7 +62,7 @@ if (!empty($_POST)) {
                     </div>
                     <div class="item">
                         <h3 class="item-title">会社名</h3>
-                        <div class="form-wrapper"><?= $newInvoice['name']?></div>
+                        <div class="form-wrapper"><?= h($newInvoice['name'])?></div>
                     </div>
                     <div class="item">
                         <h3 class="item-title">金額<span>(半角数字)</span></h3>
