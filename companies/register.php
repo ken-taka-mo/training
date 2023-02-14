4c9936e1f7863641c4d8182ff84d22347b3605fb
user <user@localhost.localdomain>
2023-02-14 17:26:50 +0900

    空バリデーション共通化

--- companies/register.php
+++ companies/register.php
@@ -1,78 +1,10 @@
 <?php
 require_once('../utils/prefectures.php');
 require_once('../utils/functions.php');
+require_once('../utils/columns.php');
 session_start();
 
-
-if (!empty($_POST)) {
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['name'], "s"))) {
-        $error['name'] = '会社名を入力してください';
-    } elseif (mb_strlen($_POST['name']) > 64) {
-        $error['name'] = '会社名は64文字以内で入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['manager_name'], "s"))) {
-        $error['manager_name'] = '担当者名を入力してください';
-    } elseif (mb_strlen($_POST['manager_name']) > 32) {
-        $error['manager_name'] = '担当者名は32文字以内で入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['phone_number'], "s"))) {
-        $error['phone_number'] = '電話番号を入力してください';
-    } elseif (mb_strlen($_POST['phone_number']) > 11 || !preg_match('/^\d+$/', $_POST['phone_number'])) {
-        $error['phone_number'] = '電話番号はハイフンなしの11桁以下の半角整数で入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['postal_code'], "s"))) {
-        $error['postal_code'] = '郵便番号を入力してください';
-    } elseif (!preg_match('/^\d{7}$/', $_POST['postal_code'])) {
-        $error['postal_code'] = '郵便番号はハイフンなしの7桁の半角整数で入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['prefecture_code'], "s"))) {
-        $error['prefecture_code'] = '都道府県を選択してください';
-    } elseif (mb_strlen($_POST['prefecture_code']) < 1 && mb_strlen($_POST['prefecture_code'] > 47)) {
-        $error['prefecture_code'] = 'もう一度都道府県を選択してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['address'], "s"))) {
-        $error['address'] = '市区町村を入力してください';
-    } elseif (mb_strlen($_POST['address']) > 100) {
-        $error['address'] = '市区町村は100字以内で入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['mail_address'], "s"))) {
-        $error['mail_address'] = 'メールアドレスを入力してください';
-    } elseif (mb_strlen($_POST['mail_address']) > 100) {
-        $error['mail_address'] = 'メールアドレスは100字以内で入力して下さい';
-    } elseif (!preg_match('/^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*$/', $_POST['mail_address'])) {
-        $error['mail_address'] = '正しいメールアドレスを入力してください';
-    }
-
-    if (preg_match('/^[\s]*$/', mb_convert_kana($_POST['prefix'], "s"))) {
-        $error['prefix'] = 'プレフィックスを入力してください';
-    } elseif (!preg_match('/^[a-zA-Z0-9]{1,8}$/', $_POST['prefix'])) {
-        $error['prefix'] = 'プレフィックスは8字以内の半角英数字で入力してください';
-    }
-
-    if (!isset($error)) {
-        $_POST['name'] = preg_replace("/(^\s+)|(\s+$)/u", "", $_POST['name']);
-        $_POST['name'] = mb_convert_kana($_POST['name'], "n");
-        $_POST['manager_name'] = preg_replace("/(^\s+)|(\s+$)/u", "", $_POST['manager_name']);
-        $_POST['manager_name'] = mb_convert_kana($_POST['manager_name'], "n");
-        $_POST['address'] = preg_replace("/(^\s+)|(\s+$)/u", "", $_POST['address']);
-        $_POST['address'] = mb_convert_kana($_POST['address'], "n");
-        $_SESSION['register'] = $_POST;
-        header('Location: check.php');
-        exit();
-    }
-}
-
-if (isset($_GET['action']) && $_GET['action'] == 'rewrite') {
-    $_POST = $_SESSION['register'];
-}
-
-
+// 各入力データを初期化（フォームの初期値））
 $name = '';
 $managerName = '';
 $phoneNumber = '';
@@ -82,15 +14,76 @@ $address = '';
 $mailAddress = '';
 $prefix = '';
 
+$items = [];
 if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $managerName = $_POST['manager_name'];
-    $phoneNumber = $_POST['phone_number'];
-    $postalCode = $_POST['postal_code'];
-    $prefectureCode = intval($_POST['prefecture_code']);
-    $address = $_POST['address'];
-    $mailAddress = $_POST['mail_address'];
-    $prefix = $_POST['prefix'];
+    // 入力された値の全角スペース、全角数字を半角に変換
+    foreach ($_POST as $key => $value) {
+        $items[$key] = preg_replace("/(^\s+)|(\s+$)/u", "", $value);
+        $items[$key] = mb_convert_kana($items[$key], "n");
+    }
+
+    if (mb_strlen($items['name']) > 64) {
+        $error['name'] = '会社名は64文字以内で入力してください';
+    }
+    if (mb_strlen($items['manager_name']) > 32) {
+        $error['manager_name'] = '担当者名は32文字以内で入力してください';
+    }
+
+    if (!preg_match('/^\d{1,11}$/', $items['phone_number'])) {
+        $error['phone_number'] = '電話番号は11桁以下の整数で入力してください';
+    }
+
+    if (!preg_match('/^\d{7}$/', $items['postal_code'])) {
+        $error['postal_code'] = '郵便番号は7桁の整数で入力してください';
+    }
+
+    if ($items['prefecture_code'] < 1 && $items['prefecture_code'] > 47) {
+        $error['prefecture_code'] = 'もう一度都道府県を選択してください';
+    }
+
+    if (mb_strlen($items['address']) > 100) {
+        $error['address'] = '市区町村は100字以内で入力してください';
+    }
+
+    if (mb_strlen($items['mail_address']) > 100) {
+        $error['mail_address'] = 'メールアドレスは100字以内で入力して下さい';
+    } elseif (!preg_match('/^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*$/', $items['mail_address'])) {
+        $error['mail_address'] = '正しいメールアドレスを入力してください';
+    }
+
+    if (!preg_match('/^[a-zA-Z0-9]{1,8}$/', $items['prefix'])) {
+        $error['prefix'] = 'プレフィックスは8字以内の英数字で入力してください';
+    }
+    // スペース以外の値が入っているか＝入力されているか
+    foreach ($items as $key => $value) {
+        if (preg_match('/^[\s]*$/', $value)) {
+            $error[$key] = COLUMNS[$key] . 'を入力してください';
+        }
+    }
+    // バリデーションチェックで問題がなかった場合セッションに値を代入し確認ページへ遷移
+    if (!isset($error)) {
+        $_SESSION['register'] = $items;
+        header('Location: check.php');
+        exit();
+    }
+}
+
+// 確認ページからの書き戻しによる遷移かどうかの確認
+// trueだった場合セッションの値をitemsに代入
+if (isset($_GET['action']) && $_GET['action'] == 'rewrite') {
+    $items = $_SESSION['register'];
+}
+
+// inputに表示する値の代入
+if (!empty($items)) {
+    $name = $items['name'];
+    $managerName = $items['manager_name'];
+    $phoneNumber = $items['phone_number'];
+    $postalCode = $items['postal_code'];
+    $prefectureCode = intval($items['prefecture_code']);
+    $address = $items['address'];
+    $mailAddress = $items['mail_address'];
+    $prefix = $items['prefix'];
 }
 
 ?>
@@ -128,7 +121,7 @@ if (!empty($_POST)) {
                         <p class="error"><?= $error['manager_name'] ?></p>
                     <?php endif ?>
                     <div class="item">
-                        <h3 class="item-title">電話番号<span>(半角)</span></h3>
+                        <h3 class="item-title">電話番号</h3>
                         <div class="form-wrapper"><input type="text" name="phone_number" class="tel-input" maxlength="11" value=<?= h($phoneNumber) ?>></div>
                     </div>
                     <?php if (isset($error['phone_number'])) :?>
@@ -138,8 +131,9 @@ if (!empty($_POST)) {
                         <h3 class="item-title">住所</h3>
                         <div class="address-item-wrapper">
                             <div class="address-item">
-                                <h4>郵便番号<span>(半角)</span></h4>
+                                <h4>郵便番号</h4>
                                 <input type="text" name="postal_code" class="short-input" maxlength="7" value=<?= h($postalCode) ?>>
+                                <span>(ハイフンなし)</span>
                             </div>
                             <div class="address-item">
                                 <h4>都道府県</h4>
